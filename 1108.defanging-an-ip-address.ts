013e4c0dd6cad72f0628965a6e0b35441a3e5196
Juan Francisco Barrón Camacho <franz.barron@outlook.com>
2021-11-19 11:10:30 -0600

    Add solution to 1108. Defanging an IP Address

---
+++ 1108.defanging-an-ip-address.ts
@@ -0,0 +1,11 @@
+/*
+ * @lc app=leetcode id=1108 lang=typescript
+ *
+ * [1108] Defanging an IP Address
+ */
+
+// @lc code=start
+function defangIPaddr(address: string): string {
+  return address.replace(/\./g, "[.]");
+}
+// @lc code=end
